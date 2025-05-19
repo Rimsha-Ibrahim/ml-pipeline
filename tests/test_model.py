71b329e6fe6b5bda670282a0ae64e163291654d8
Rimsha Ibrahim <145927661+Rimsha-Ibrahim@users.noreply.github.com>
2025-05-19 11:30:25 +0500

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,5 @@
 from src.train_model import train_and_save_model
 
 def test_model_accuracy():
-    """Ensure accuracy > 80%"""
     accuracy = train_and_save_model()
-    assert accuracy > 0.8, f"Accuracy {accuracy} is too low!"
+    assert accuracy > 0.8
